4d89196648b4b859a8bcc5fc66b695efb0f17d9d
L0818 <2430650987@qq.com>
2019-10-24 09:47:54 +0800

    李小玲提交

--- common/Week02.java
+++ common/Week02.java
@@ -3,7 +3,12 @@ public class Week02{
 	public static void HuLiuzhen(){
 		System.out.println("Hu Liuzhen");
 	}
+        public static void XiaolingLi()	{
+		System.out.peintln("Xiaoling Li");
+	}
 	public static void main(String [] args){
 		HuLiuzhen();
+		XiaolingLi();
 	}
+
 }
