dd8acf117a8a5417f9cdabc456b669ae6d4b2398
Hunkchen <2103470608@qq.com>
2019-10-27 21:45:21 +0800

    提交

--- common/Week02.java
+++ common/Week02.java
@@ -12,6 +12,8 @@ public class Week02 {
     public static void XiaolingLi() {
         System.out.println("Xiaoling Li");
     }
+    public static void Chenjiebin(){
+	System.out.println("Chenjiebin"); }
 
     public static void ZhiweiZheng() {
         System.out.println("Zhiwei Zheng");
@@ -24,6 +26,7 @@ public class Week02 {
     public static void main(String[] args) {
         HuLiuzhen();
         XiaolingLi();
+	Chenjiebin();
         ZhiweiZheng();
         LiBaoHong();
     }
