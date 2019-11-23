5a790ea2ea472d1cc249b510fd90bc0d28bad54e
XinyaoChen2991008 <56512720+XinyaoChen2991008@users.noreply.github.com>
2019-11-23 19:06:17 +0800

    Delete 第二题.py

--- 第二题.py
+++
@@ -1,7 +0,0 @@
-#编写程序，输入本金、年利率和年数，计算复利（结果保留两位小数）
-
-money = int(input("请输入本金："))
-rate = float(input("请输入年利率："))
-years = int(input("请输入年数："))
-amount = money*((1+rate/100)**years)
-print(str.format("本金利率和为：{0:2.2f}",amount))
